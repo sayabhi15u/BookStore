03f6498b8fb284d5c01874a9ac6ec222ba46820a
Sumit Chaudhury <sumitchaudhury@deloitte.co.uk>
2022-09-19 19:13:38 +0530

    Variable name fix

--- BookStore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookStore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -209,7 +209,7 @@ namespace BookStore.Areas.Identity.Pages.Account
                     Text = i.Name,
                     Value = i.Id.ToString()
                 }),
-                RoleList = _roleManager.Roles.Where(u => u.Name != SD.Role_User_Indi).Select(x => x.Name).Select(i => new SelectListItem
+                RoleList = _roleManager.Roles.Where(u => u.Name != SD.Role_User).Select(x => x.Name).Select(i => new SelectListItem
                 {
                     Text = i,
                     Value = i
