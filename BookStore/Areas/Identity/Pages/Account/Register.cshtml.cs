1a773a6dbc6f196e72a776304a3e3d0303d548b2
AbhishekKumarSingh <abhishekkumarsingh@deloitte.co.uk>
2022-09-19 12:34:45 +0000

    Updated Code

--- BookStore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookStore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -205,6 +205,7 @@ namespace BookStore.Areas.Identity.Pages.Account
 
             // If we got this far, something failed, redisplay form
             return Page();
+           
         }
     }
 }
